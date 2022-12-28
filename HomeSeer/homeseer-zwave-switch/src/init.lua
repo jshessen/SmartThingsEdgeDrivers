463d207f01c576756a2cbb6fb36502be4b78ec27
Ryan Mulder <ryan.mulder@antrum.com>
2022-12-28 11:39:43 -0500

    basic features work, click events work

--- HomeSeer/homeseer-zwave-switch/src/init.lua
+++ HomeSeer/homeseer-zwave-switch/src/init.lua
@@ -1,4 +1,6 @@
--- Copyright 2022 Ryan Mulder
+-- Author: ryanjmulder
+--
+-- Copyright 2022 SmartThings
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -14,38 +16,65 @@
 
 local log = require "log"
 local capabilities = require "st.capabilities"
-local Driver = require "st.driver"
+local defaults = require "st.zwave.defaults"
+local cc = require "st.zwave.CommandClass"
+local SwitchBinary = require "st.zwave.CommandClass.SwitchBinary"
+local ZwaveDriver = require "st.zwave.driver"
+local Basic = (require "st.zwave.CommandClass.Basic")({ version = 1 })
+local CentralScene = (require "st.zwave.CommandClass.CentralScene")({ version = 1 })
+local constants = require "st.zwave.constants"
 
-local function handle_on(driver, device, command)
-  log.info("Send on command to device")
 
-  -- for real devices you should make a request to a device and wait for the
-  -- response confirming the device was switched on before emitting this
-  device:emit_event(capabilities.switch.switch.on())
-end
-
-local function handle_off(driver, device, command)
-  log.info("Send off command to device")
-
-  -- for real devices you should make a request to a device and wait for the
-  -- response confirming the device was switched off before emitting this
-  device:emit_event(capabilities.switch.switch.off())
-end
-
--- Driver library initialization
-local example_driver =
-  Driver("example_driver",
-    {
-      capability_handlers = {
-      [capabilities.switch.ID] =
-      {
-        [capabilities.switch.commands.on.NAME] = handle_on,
-        [capabilities.switch.commands.off.NAME] = handle_off
-      }
-    }
+local map_key_attribute_to_capability = {
+  [CentralScene.key_attributes.KEY_PRESSED_1_TIME] = {
+    [0x01] = capabilities.button.button.up(),
+    [0x02] = capabilities.button.button.down()
+  },
+  [CentralScene.key_attributes.KEY_PRESSED_2_TIMES] = {
+    [0x01] = capabilities.button.button.up_2x(),
+    [0x02] = capabilities.button.button.down_2x()
+  },
+  [CentralScene.key_attributes.KEY_PRESSED_3_TIMES] = {
+    [0x01] = capabilities.button.button.up_3x(),
+    [0x02] = capabilities.button.button.down_3x()
+  },
+  [CentralScene.key_attributes.KEY_PRESSED_4_TIMES] = {
+    [0x01] = capabilities.button.button.up_4x(),
+    [0x02] = capabilities.button.button.down_4x()
+  },
+  [CentralScene.key_attributes.KEY_PRESSED_5_TIMES] = {
+    [0x01] = capabilities.button.button.up_5x(),
+    [0x02] = capabilities.button.button.down_5x()
   }
-)
+}
 
--- Put other setup code here
+local function central_scene_notification_handler(driver, device, cmd)
+  if device:get_field("last_sequence_number") ~= cmd.args.sequence_number then
+    device:set_field("last_sequence_number", cmd.args.sequence_number)
+    local event_map = map_key_attribute_to_capability[cmd.args.key_attributes]
+    local event = event_map and event_map[cmd.args.scene_number]
+    if event ~= nil then
+      device:emit_event_for_endpoint(cmd.src_channel, event)
+    end
+  end
+end
 
-example_driver:run()
+local driver_template = {
+  supported_capabilities = {
+    capabilities.switch,
+    capabilities.switchLevel,
+    capabilities.fanSpeed,
+    capabilities.refresh,
+    capabilities.button
+  },
+  zwave_handlers = {
+    [cc.CENTRAL_SCENE] = {
+      [CentralScene.NOTIFICATION] = central_scene_notification_handler
+    }
+  },
+  NAME = "homeseer zwave",
+}
+
+defaults.register_for_default_handlers(driver_template, driver_template.supported_capabilities)
+local homeseer_switch = ZwaveDriver("homeseer-zwave-switch", driver_template)
+homeseer_switch:run()
