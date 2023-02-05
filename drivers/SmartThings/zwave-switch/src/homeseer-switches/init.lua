de92fda20b558b5da24a7336f92d061ba372bf7e
jshessen <jeff.hessenflow@gmail.com>
2023-02-05 11:38:18 -0600

    test(version): correcte debug typo

--- drivers/SmartThings/zwave-switch/src/homeseer-switches/init.lua
+++ drivers/SmartThings/zwave-switch/src/homeseer-switches/init.lua
@@ -620,7 +620,7 @@ local function info_changed(self, device, event, args)
         -- We may need to update our device profile
         local report = Version.Report
         log.debug(string.format("%s [%s] : dump=", device.id, device.device_network_id))
-        log.deubt(dump(report))
+        log.debug(dump(report))
       update_device_profile(self, device, report)
     end
   -- Call the topmost 'infoChanged' lifecycle hander to do any default work
