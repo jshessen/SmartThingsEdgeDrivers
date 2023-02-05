c634d43921b13d2fac3c6ccd072decf8af0a3453
jshessen <jeff.hessenflow@gmail.com>
2023-02-05 11:19:30 -0600

    test(version): Modify debug output

--- drivers/SmartThings/zwave-switch/src/homeseer-switches/init.lua
+++ drivers/SmartThings/zwave-switch/src/homeseer-switches/init.lua
@@ -593,7 +593,7 @@ local function info_changed(self, device, event, args)
     if args.old_st_store.preferences.operatingMode ~= device.preferences.operatingMode then
         -- We may need to update our device profile
         local report = Version.Report
-        log.debug(string.format("%s [%s] : firmware_sub_version=%s", device.id, device.device_network_id, report.args.firmware_0_sub_version))
+        log.debug(string.format("%s [%s] : firmware_sub_version=%s", device.id, device.device_network_id, report))
       update_device_profile(self, device, report)
     end
   -- Call the topmost 'infoChanged' lifecycle hander to do any default work
