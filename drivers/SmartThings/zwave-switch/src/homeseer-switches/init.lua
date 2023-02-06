604dc88130c8b19fbc1dc70194ea5cf6d85942f6
jshessen <jeff.hessenflow@gmail.com>
2023-02-05 19:08:17 -0600

    test(switch): Add log.debug

--- drivers/SmartThings/zwave-switch/src/homeseer-switches/init.lua
+++ drivers/SmartThings/zwave-switch/src/homeseer-switches/init.lua
@@ -149,9 +149,9 @@ local function set_status_led(device,id, value, color)
   log.debug(string.format("%s [%s] : value=%s", device.id, device.device_network_id, value))
   log.debug(string.format("%s [%s] : color=%s", device.id, device.device_network_id, color))
   for id, value in pairs(device.preferences) do
-    log.debug(string.format("%s [%s] : %s = '%s'", device.id, device.device_network_id, id, value))
+    log.debug(string.format("%s [%s] : %s='%s'", device.id, device.device_network_id, id, value))
   end
-  local new_color = device:get_field("ledStatusColor1")
+  local new_color = preferencesMap.to_numeric_value(device.preferences[id])
   log.debug(string.format("%s [%s] : get_field(%s)=%s", device.id, device.device_network_id, id, new_color))
 
   if preferences and preferences[id] then
