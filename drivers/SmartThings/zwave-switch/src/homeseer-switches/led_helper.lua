fe61dc6f8ca5c52f7b991b363ae284c8337ed2f2
jshessen <jeff.hessenflow@gmail.com>
2023-02-17 11:52:23 -0600

    refactor(led): remove debug log

--- drivers/SmartThings/zwave-switch/src/homeseer-switches/led_helper.lua
+++ drivers/SmartThings/zwave-switch/src/homeseer-switches/led_helper.lua
@@ -58,60 +58,72 @@ local led = {}
 ---
 
 --- @function led.set_status_color() --
---- Sets component color to closes supported color match
+--- Sets component color to closest supported color match
 --- @param device (st.zwave.Device) The device object
---- @param command (table) Input command value
+--- @param command (Command) Input command value
 --- @return (nil)
 function led.set_status_color(device, command)
-  -- Retrieve the on/off value from the arguments
+  ---@type number
   local value = command.args.value
-  -- Retrieve the device parameters from the preferences map
-  local preferences = preferencesMap.get_device_parameters(device)
-  -- Construct the component name by concatenating the string "ledStatusColor" with the number extracted from the command component
+  ---@type string
   local component = "ledStatusColor" .. string.sub(command.component, string.find(command.component, "-") + 1)
-  -- Set default color value to White
+  ---@type table
   local color = helpers.color.map[7]
-  
-  if value == SwitchBinary.value.ON_ENABLE then
-    if device:supports_capability(capabilities.colorControl,nil) then
-      -- If the color argument is present in the command
-      if command.args.color then
-        -- Find the closest color based on hue, saturation, and lightness values
-        color = helpers.color.find_closest_color(command.args.color.hue, command.args.color.saturation, command.args.color.lightness)
-        log.debug(string.format("%s: Brought my own color; color.value=%s",device:pretty_print(), color.value))
 
-      -- If color is not defined, check the device.preferences, if neither is defined set to White=7
-      elseif preferences and preferences[component] then
-        color = helpers.color.map[ device.preferences[component] and tonumber(device.preferences[component]) or 7 ]
-      end
-        
-      -- Convert the color from hex to RGB and then to HSL
-      local r, g, b = helpers.color.hex_to_rgb(color.hex)
-      local hue, saturation, lightness = utils.rgb_to_hsl(r,g,b)
-      
-      -- Update the hue and saturation values in the command arguments
-      command.args.color = {
-        hue = hue,
-        saturation = saturation
-      }
-      device:set_field(CAP_CACHE_KEY, command)
-      
-      -- Determine the value of the status LED based on hue and saturation values
-      value = saturation == 0 and lightness == 0 and SwitchBinary.value.OFF_DISABLE or SwitchBinary.value.ON_ENABLE
+  if value == SwitchBinary.value.ON_ENABLE and device:supports_capability(capabilities.colorControl, nil) then
+    if command.args.color then
+      color = helpers.color.find_closest_color(command.args.color.hue, command.args.color.saturation, command.args.color.lightness)
+    else
+      color = helpers.color.map[device.preferences[component] and tonumber(device.preferences[component]) or 7]
     end
+
+    ---@type number|nil
+    local r, g, b = helpers.color.hex_to_rgb(color.hex)
+    if not r then
+      log.error(string.format("%s: helpers.color.hex_to_rgb returned nil for color.hex = %s", device:pretty_print(), color.hex))
+    end
+    ---@type number
+    local hue, saturation, lightness = utils.rgb_to_hsl(r, g, b)
+
+    if not command.args.color then
+      command.args.color = {}
+    end
+    command.args.color.hue = hue
+    command.args.color.saturation = saturation
+    device:set_field(CAP_CACHE_KEY, command)
+
+    --- If the saturation and lightness values are 0, set the value to off
+    value = saturation == 0 and lightness == 0 and SwitchBinary.value.OFF_DISABLE or SwitchBinary.value.ON_ENABLE
   end
-  -- Set value to OFF_DISABLE, or color value
+
+  --- Set the value to off or the color value
   value = value == SwitchBinary.value.OFF_DISABLE and value or color.value
-  log.debug(string.format("%s: color value=%s",device:pretty_print(), value))
-  
-  -- Create the configuration set based on the device parameters and value
+
+  --- Get the parameter number and size from the device preferences
+  ---@type number
+  local parameter_number = device.preferences[component] and device.preferences[component].parameter_number
+  ---@type number
+  local size = device.preferences[component] and device.preferences[component].size
+
+  if not parameter_number or not size then
+    --- If the parameter number or size is missing, log an error and return
+    log.error(string.format("%s: Missing parameter number or size for component %s", device:pretty_print(), component))
+    return
+  end
+
+  --- Create a configuration set based on the parameter number, size, and value
   local set = Configuration:Set({
-    parameter_number = preferences[component].parameter_number, 
-    size = preferences[component].size,
+    parameter_number = parameter_number,
+    size = size,
     configuration_value = value
   })
-  -- Send the configuration set to the device
-  device:send(set)
+
+  --- Send the configuration set to the device
+  local success, err = device:send(set)
+  if not success then
+    --- If there was an error sending the configuration set, log an error
+    log.error(string.format("%s: Error sending configuration set: %s", device:pretty_print(), err))
+  end
 end
 ---
 --- #######################################################
@@ -134,21 +146,24 @@ function led.set_blink_bitmask(device)
   for id = 1, count do
     local blink_id = "ledStatusBlink" .. id
     blink_enabled = device.preferences[blink_id] and (blink_freq ~= 0)
-    log.debug(string.format("%s: %s=%s", device:pretty_print(), blink_id, blink_enabled))
     if blink_enabled then
       bitmask = bitmask | (1 << (id - 1))
     end
     device:set_field(blink_id,blink_enabled)
   end
-  
+
   local set = Configuration:Set({
     parameter_number = preferences[blink_ctrl].parameter_number,
     size = preferences[blink_ctrl].size,
     configuration_value = bitmask
   })
 
-  device:send(set)
+  local success, err = device:send(set)
+  if not success then
+    error(string.format("%s: Error sending configuration set: %s", device:pretty_print(), err))
+  end
 end
+
 ---
 --- #######################################################
 ---
