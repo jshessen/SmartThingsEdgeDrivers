9a915cbfb1fd0a41b3e934dce7929f5b6e0bdf75
jshessen <jeff.hessenflow@gmail.com>
2023-02-17 17:28:16 -0600

    fix(led): preferences reference

--- drivers/SmartThings/zwave-switch/src/homeseer-switches/led_helper.lua
+++ drivers/SmartThings/zwave-switch/src/homeseer-switches/led_helper.lua
@@ -63,6 +63,7 @@ local led = {}
 --- @param command (Command) Input command value
 --- @return (nil)
 function led.set_status_color(device, command)
+  local preferences = preferencesMap.get_device_parameters(device)
   ---@type number
   local value = command.args.value
   ---@type string
@@ -101,9 +102,9 @@ function led.set_status_color(device, command)
 
   --- Get the parameter number and size from the device preferences
   ---@type number
-  local parameter_number = device.preferences[component] and device.preferences[component].parameter_number
+  local parameter_number = preferences[component] and preferences[component].parameter_number
   ---@type number
-  local size = device.preferences[component] and device.preferences[component].size
+  local size = preferences[component] and preferences[component].size
 
   if not parameter_number or not size then
     --- If the parameter number or size is missing, log an error and return
